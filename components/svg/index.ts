968b13d336298620d6971de85782199a24ce2c4e
Ryan Houk <houk.ryan@gmail.com>
2022-12-03 12:30:52 -0800

    Layout updates
    - added social media
    - updated assets
    - added icons
    -

--- components/svg/index.ts
+++ components/svg/index.ts
@@ -6,4 +6,11 @@ export { default as ArrowDownSVG } from "./ArrowDown";
 export { default as CrossSVG } from "./Cross";
 export { default as CloseSVG } from "./Close";
 export { default as ShapeTopLeftSVG } from "./ShapeTopLeft";
+export { default as TwitterSVG } from "./Twitter";
+export { default as MediumSVG } from "./Medium";
+export { default as DribbbleSVG } from "./Dribbble";
+export { default as LinkedInSVG } from "./LinkedIn";
+
+
+
 
