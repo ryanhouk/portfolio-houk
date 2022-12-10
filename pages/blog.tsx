538d408eccb976a5794fd28ffce180ee06b9256f
Ryan Houk <houk.ryan@gmail.com>
2022-12-10 09:31:55 -0800

    Updates
    - Added headless ui lib
    - Modal component added
    - Tweaks to copy
    - Blog page base added
    - Box image changed

---
+++ pages/blog.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {}
+
+const Blog = (props: Props) => {
+    return (
+        <>
+            <section className=''>
+                <h1 className="text-5xl sm:text-9xl xl:text-[200px] font-black px-12 pt-16 lg:pt-52 pb-4 lg:pb-12 text-brand-dark text-center">
+                    The blog.
+                </h1>
+            </section>
+        </>
+    )
+}
+
+export default Blog;
