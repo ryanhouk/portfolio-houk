034d12bbbb239c91fa6392eb7188e5f5385bdd54
Ryan Houk <houk.ryan@gmail.com>
2024-06-11 13:01:22 -0700

    fixed bug

--- components/sections/FeatureProject.tsx
+++ components/sections/FeatureProject.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import { FadeY } from '../animations'
-import DoodleScribbleAnimate from '../svg/DoodleScribbleAnimate'
 
 type Props = {}
 
@@ -9,27 +7,6 @@ const FeatureProject = (props: Props) => {
   return (
     <>
       {/* INTRO */}
-      <section
-        id="Work"
-        className="relative z-20 flex flex-col justify-center max-w-5xl px-8 py-12 mx-auto scroll-mt-10 selection:bg-orange-200 lg:pt-32">
-        <FadeY>
-          <DoodleScribbleAnimate
-            startColor={'#dc2626'}
-            endColor={'#facc15'}
-            className="w-24 mb-2 lg:mx-auto lg:mb-0 lg:w-44"
-          />
-        </FadeY>
-        <h1 className="z-10 mb-4 text-4xl text-white font-clash lg:mb-8 lg:text-center lg:text-9xl">
-          <FadeY delay={0.25}>Work </FadeY>
-          <FadeY delay={0.5}>Highlights</FadeY>
-        </h1>
-        <FadeY className="relative z-20" delay={0.75}>
-          <p className="max-w-2xl mx-auto font-mono text-sm leading-loose text-white opacity-60 lg:text-center">
-            There is a lot of work to choose from! Here are a few highlights to
-            dive into.
-          </p>
-        </FadeY>
-      </section>
 
       {/* CONTENT */}
       <section className="relative py-12 lg:pb-32">
