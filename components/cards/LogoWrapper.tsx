d59c8404a1360fe47d360993874f7b51d4d3f320
Ryan Houk <houk.ryan@gmail.com>
2024-06-13 18:34:48 -0700

    KL Case study updates

--- components/cards/LogoWrapper.tsx
+++ components/cards/LogoWrapper.tsx
@@ -2,12 +2,14 @@ import React, { ReactNode } from 'react'
 
 type Props = {
   children?: ReactNode
+  className?: string
 }
 
-const LogoWrapper = ({ children }: Props) => {
+const LogoWrapper = ({ children, className }: Props) => {
   return (
     <>
-      <div className="p-[1px] bg-gradient-to-b from-white/10 to-black rounded-3xl overflow-clip transition-all duration-500">
+      <div
+        className={`${className} p-[1px] bg-gradient-to-b from-white/10 to-black rounded-3xl overflow-clip transition-all duration-500`}>
         <div className="relative flex items-center justify-center w-full h-48 transition-all bg-black group hover:cursor-cell rounded-[23.5px]">
           {children}
         </div>
