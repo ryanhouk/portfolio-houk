2bead79d5fe2b0b54b5f6f69e51d62f375df3481
Ryan Houk <houk.ryan@gmail.com>
2024-08-10 11:40:29 -0700

    Updated hero carousel component

--- components/heroes/HeroCarousel.tsx
+++ components/heroes/HeroCarousel.tsx
@@ -29,12 +29,12 @@ const cards: CardData[] = [
   {
     image: '/img/mockup/klove/web/taya-books.webp',
     altText: 'Description for image 3',
-    imageClass: 'mx-auto w-[1000px] lg:w-full -ml-12 max-w-screen-3xl',
+    imageClass: 'mx-auto w-[1000px] lg:w-full -ml-12 -mt-24 max-w-screen-3xl',
   },
   {
     image: '/img/mockup/klove/web/emf-mob.webp',
     altText: 'Description for image 2',
-    imageClass: 'mx-auto w-[1000px] lg:scale-100 w-full max-w-screen-3xl',
+    imageClass: 'mx-auto w-[1000px] lg:w-full max-w-screen-3xl',
   },
 ]
 
@@ -60,23 +60,14 @@ const HeroCarousel = () => {
   return (
     <>
       {/* BUTTONS */}
-      <div className="flex flex-col items-center justify-center gap-4">
-        <div className="flex gap-4">
-          <button
-            onClick={handlePrev}
-            className="p-[1px] group rounded-full bg-gradient-to-b from-zinc-800 to-zinc-950 transition-all hover:-translate-x-1 duration-300">
-            <div className="flex items-center justify-center w-12 h-12 font-mono text-sm text-white transition-all duration-300 rounded-full group-hover:brightness-150 bg-gradient-to-b from-zinc-900 to-zinc-950">
-              <ArrowRightSquare className={'w-6 rotate-180'} />
-            </div>
-          </button>
-          <button
-            onClick={handleNext}
-            className="p-[1px] group rounded-full bg-gradient-to-b from-zinc-800 to-zinc-950 transition-all hover:translate-x-1 duration-300">
-            <div className="flex items-center justify-center w-12 h-12 font-mono text-sm text-white transition-all duration-300 rounded-full group-hover:brightness-150 bg-gradient-to-b from-zinc-900 to-zinc-950">
-              <ArrowRightSquare className={'w-6'} />
-            </div>
-          </button>
-        </div>
+      <div className="flex items-center justify-center gap-4">
+        <button
+          onClick={handlePrev}
+          className="p-[1px] group rounded-full bg-gradient-to-b from-zinc-800 to-zinc-950 transition-all hover:-translate-x-1 duration-300">
+          <div className="flex items-center justify-center w-12 h-12 font-mono text-sm text-white transition-all duration-300 rounded-full group-hover:brightness-150 bg-gradient-to-b from-zinc-900 to-zinc-950">
+            <ArrowRightSquare className={'w-6 rotate-180'} />
+          </div>
+        </button>
         <div className="flex gap-2">
           {cards.map((_, index) => (
             <button
@@ -86,6 +77,13 @@ const HeroCarousel = () => {
             />
           ))}
         </div>
+        <button
+          onClick={handleNext}
+          className="p-[1px] group rounded-full bg-gradient-to-b from-zinc-800 to-zinc-950 transition-all hover:translate-x-1 duration-300">
+          <div className="flex items-center justify-center w-12 h-12 font-mono text-sm text-white transition-all duration-300 rounded-full group-hover:brightness-150 bg-gradient-to-b from-zinc-900 to-zinc-950">
+            <ArrowRightSquare className={'w-6'} />
+          </div>
+        </button>
       </div>
       {/* IMAGES */}
       <AnimatePresence mode="wait">
