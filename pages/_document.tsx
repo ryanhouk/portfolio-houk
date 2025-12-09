4a0898f652f898a41abeba982b54c95c768c9e88
Ryan Houk <houk.ryan@gmail.com>
2025-12-09 09:13:59 -0800

    Font fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,12 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;300;400&display=swap"
           rel="stylesheet"
-        /> */}
+        />
       </Head>
       <body className='scroll-smooth'>
         <Main />
